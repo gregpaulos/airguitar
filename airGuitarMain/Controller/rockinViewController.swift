80fde132547a232c6253a7a663297c4b64d31b87
gregory paulos <gregorypaulos@gregorys-MacBook-Air.local>
2018-05-18 15:22:21 -0600

    cleaned up rockinViewController code

--- airGuitarMain/Controller/rockinViewController.swift
+++ airGuitarMain/Controller/rockinViewController.swift
@@ -20,13 +20,21 @@ class rockinViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
     
     var multiplePlayersAtOnce = [AVAudioPlayer]()
     
-    var count: Int = 0
-    
     var notHandAfterHand = 0
     
     var chordCount = 0
     
- 
+    // video capture session
+    let session = AVCaptureSession()
+    // queue for processing video frames
+    let captureQueue = DispatchQueue(label: "captureQueue")
+    // preview layer
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    // preview view - we'll add the previewLayer to this later
+    
+    // vision request
+    var visionRequests = [VNRequest]()
+
     
     @IBAction func pressDismiss(_ sender: Any) {
         objPlayer?.stop()
@@ -43,58 +51,19 @@ class rockinViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
     
     @IBOutlet weak var handsProbabilityLabel: UILabel!
     
-
-        // video capture session
-        let session = AVCaptureSession()
-        // queue for processing video frames
-        let captureQueue = DispatchQueue(label: "captureQueue")
-        // preview layer
-        var previewLayer: AVCaptureVideoPreviewLayer!
-        // preview view - we'll add the previewLayer to this later
-
-
-//
-//
-//    @IBAction func chooseDifferentSongPressed(_ sender: UIButton) {
-//        objPlayer?.stop()
-//        if multiplePlayersAtOnce.count > 0 {
-//            for player in multiplePlayersAtOnce {
-//                player.stop()
-//            }
-//        }
-//        session.stopRunning()
-//        dismiss(animated: true, completion: nil)
-//    }
-
-
-    // vision request
-    var visionRequests = [VNRequest]()
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("JUST GOT HERE")
-        print(songName)
-        print(allSongs.dictOfSongs[songName]!.songPattern)
-        
         songNameLabel.text = songName
-        
-
-
-        // Do any additional setup after loading the view.
         
         guard let camera = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .front) else {
             fatalError("No video camera available")
         }
 
-
-
         session.sessionPreset = .high
 
         // add the preview layer
         previewLayer = AVCaptureVideoPreviewLayer(session: session)
-//        previewView.layer.addSublayer(previewLayer)
 
         // create the capture input and the video output
         guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
@@ -117,20 +86,13 @@ class rockinViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         // Start the session
         session.startRunning()
 
-        // set up the vision model
-        //        guard let visionModel = try? VNCoreMLModel(for: Resnet50().model) else {
-        //            fatalError("Could not load model")
-        //        }
-
         guard let visionModel = try? VNCoreMLModel(for: secondHand().model) else {
             fatalError("Could not load model")
         }
 
         // set up the request using our vision model
         let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassifications)
-        //        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOptionCenterCrop
 
-        //        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop(1)
         classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop
 
         visionRequests = [classificationRequest]
@@ -145,7 +107,6 @@ class rockinViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-//        previewLayer.frame = self.previewView.bounds;
     }
 
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
@@ -177,45 +138,19 @@ class rockinViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
             print("No results")
             return
         }
-        //        let classifications = observations[0...4] // top 4 results
-        //            .flatmap({ $0 as? VNClassificationObservation })
-        //            .map({ "\($0.identifier) \(($0.confidence * 100.0).rounded())" })
-        //            .joined(separator: "\n")
 
         let classification = observations[0] as? VNClassificationObservation
-
-
-        //        let classificationText = "\(classification?.identifier) \(((classification?.confidence)! * 100.0).rounded())"
-        //
-        //        print("CLASSIFICATION")
-        //        print(classification)
-        //        print(classificationText)
-        //
-        //
-        //        DispatchQueue.main.async {
-        //            self.resultView.layer.zPosition = 1;
-        //            self.resultView.text = classificationText
-        //        }
-
 
         if let nonOptClassification = classification {
 
             let classificationText = "\(nonOptClassification.identifier) \(((nonOptClassification.confidence) * 100.0).rounded())"
 
-            //            print("CLASSIFICATION")
-            //            print(nonOptClassification)
-            //            print(classificationText)
-            //
-
             if nonOptClassification.identifier == "hands" && nonOptClassification.confidence > 0.80  && notHandAfterHand >= 30 {
                 playChord()
 
             }
-
-            //            perhaps drop this to less then 80
             if (nonOptClassification.identifier == "hands" && nonOptClassification.confidence < 0.50) || nonOptClassification.identifier != "hands" {
                 notHandAfterHand += 1
-//                print("Not Hand Count:", notHandAfterHand)
             }
 
             DispatchQueue.main.async {
@@ -241,13 +176,11 @@ class rockinViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         if chordCount >= songPattern.count {
             chordCount = 0
             url = songPattern[chordCount]
-            print(chordCount)
             chordCount += 1
 
         }
         else {
             url = songPattern[chordCount]
-            print(chordCount)
             chordCount += 1
         }
         
@@ -285,9 +218,7 @@ class rockinViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
                 multiplePlayersAtOnce.append(objPlayer!)
                 
                 
-                multiplePlayersAtOnce[count].play()
-                
-                count+=1
+                multiplePlayersAtOnce[multiplePlayersAtOnce.count-1].play()
                 
                 
             } catch let error {
